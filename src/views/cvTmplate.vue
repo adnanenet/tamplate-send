9c2cf9ab12f1795520bf49c0fd2cc2cd0d4e05b5
cyberjujutsu <bensouda.adnane.dev@gmail.com>
2024-06-25 23:47:41 +0100

    we have reqeust ok and we return the pdf

--- src/views/cvTmplate.vue
+++ src/views/cvTmplate.vue
@@ -31,37 +31,42 @@ const logDataToConsole = () => {
     css: cvUserData.value.css,
   };
 
-  console.log('Data to log:', JSON.stringify(dataToLog.value, null, 2)); // Log the data as a formatted JSON string
+  // console.log('Data to log:', dataToLog.value); // Log the data as a formatted JSON string
 };
 
 const createPost = async () => {
-  const postData = JSON.stringify({
+  const postData = {
     name: cvUserData.value.name,
     html: cvUserData.value.html,
     css: cvUserData.value.css
-  });
+  };
 
-  console.log('Data being sent to API:', postData); // Log the formatted JSON data
+  console.log('Data being sent to API:', JSON.stringify(postData, null, 2)); // Log the formatted JSON data
 
   try {
     const response = await axios.post('http://104.248.87.162:5000/generate-pdf', postData, {
       headers: {
         'Content-Type': 'application/json',
       },
+      responseType: 'arraybuffer'
     });
     console.log('Response from API:', response.data);
+    if (response.status === 200) {
+      const blob = new Blob([response.data], { type: response.headers['content-type'] });
+      const link = document.createElement('a');
+      link.href = URL.createObjectURL(blob);
+      link.download = 'cv.pdf';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      return 200;
+    } else {
+      console.error('Failed with status:', response.status);
+      return 500;
+    }
   } catch (error) {
     console.error('Failed to send data to API:', error);
-    if (error.response) {
-      console.error('Error response data:', error.response.data);
-      console.error('Error response status:', error.response.status);
-      console.error('Error response headers:', error.response.headers);
-    } else if (error.request) {
-      console.error('Error request data:', error.request);
-    } else {
-      console.error('Error message:', error.message);
-    }
-    console.error('Error config:', error.config);
+    return 500;
   }
 };
 
